90e7335a1c1366b7936333023b3252e5cdd8e2c9
Lucas Thépot <lucasthepot13@gmail.com>
2023-05-31 13:54:58 +0100

    added .env, cors

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/vinted");
+mongoose.connect(process.env.MONGODB_URI);
+const cors = require("cors");
+app.use(cors());
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -16,4 +18,4 @@ app.use(userRoutes);
 const offerRoutes = require("./routes/offer");
 app.use(offerRoutes);
 
-app.listen(3000, () => console.log("Server started"));
+app.listen(process.env.PORT || 3000, () => console.log("Server started"));
